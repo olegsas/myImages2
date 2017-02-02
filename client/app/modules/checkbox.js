ceb52f4de42aae35ad2127f596e157b84cfe2561
Oleg <o.sas@aisnovations.com>
2017-02-02 15:05:51 +0300

    I add cursor pointer to the checkbox controller

--- client/app/modules/checkbox.js
+++ client/app/modules/checkbox.js
@@ -6,13 +6,18 @@ angular.module('app.checkbox', ['ngFileUpload', 'bootstrapLightbox'])
 
     $scope.$watch('file', function () {
         if ($scope.file != null) {
+            var body = document.querySelector('body');// we find the body selector
+            angular.element(body).css('cursor', 'progress');
             Upload.upload({ url: '/image', data: { image: $scope.file } })
                 .then(res => {
-                    if (res.status = 200)
-                        $scope.images.push({url: res.data})
+                    if (res.status = 200) {
+                        $scope.images.push({url: res.data});
+                        angular.element(body).css('cursor', 'default');
+                    }
                 });
         }
     });
+
 
     $scope.openModal = function (img) {
         var modalInstance = $uibModal.open({
